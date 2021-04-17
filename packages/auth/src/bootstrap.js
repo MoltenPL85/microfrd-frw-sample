a47ccc656b559b8ca155b6ba3c975e75d38602b2
MoltenPL85 <bugaev.maxim.85@gmail.com>
2021-04-17 14:30:43 +0500

    Auth and path issues

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -9,6 +9,7 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
     createMemoryHistory({
       initialEntries: [initialPath],
     });
+
   if (onNavigate) {
     history.listen(onNavigate);
   }
@@ -27,7 +28,7 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
 };
 
 if (process.env.NODE_ENV === 'development') {
-  const devRoot = document.querySelector('#_marketing-dev-root');
+  const devRoot = document.querySelector('#_auth-dev-root');
 
   if (devRoot) {
     mount(devRoot, { defaultHistory: createBrowserHistory() });
